6bd57320ca58a6c89d6ae9e9b7761f5a218e24be
JulioFilizzola <juliofilizzola@hotmail.com>
2021-09-21 18:32:07 -0300

    editit Featured Movie

--- src/components/FeaturedMovie.jsx
+++ src/components/FeaturedMovie.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './style/FeaturedMovie.css'
 
 function FeaturedMovie({item}) {
+
+  let firstDate = new Date(item.first_ait_date);
+
   return (
    <section className="featured" style={{
      backgroundSize: 'cover',
@@ -10,11 +13,19 @@ function FeaturedMovie({item}) {
    }}>
       <div className="featured--vertical">
         <div className="featured--horizontal">
-          
+          <h1 className="featured--name"> {item.original_name}</h1>
+          <div className="featured--info" >
+            <span className="featured--points">{item.vote_average} pontos</span>
+            <span className="featured--year">{firstDate.getFullYear()}</span>
+            <span className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons >= 2 ? "s" : '' }</span>
+          </div>
+          <div className="featured--description">{item.overview}</div>
+          <div className="featured--buttons"></div>
+
         </div>
       </div>
    </section>
-  )
+  );
 }
 
 export default FeaturedMovie;
