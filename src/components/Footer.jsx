0db4f22d85f816d8d88d0caadec9d198aaca5203
filizzola <juliofilizzola@hotmail.com>
2021-09-25 18:14:57 -0300

    criando Footer | edição final no header

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      feito com <span role='img' aria-label="coração" >❤</span> por Julio Filizzola, seguindo o tutorial do B7Web <br/>
+      Diereitos de imagem para Netflix.<br/>
+      Dados obtidos no site Themoviedb.org.
+    </footer>
+  );
+}
+
+export default Footer;
