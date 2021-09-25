172d058440582d3f65a198066d435714cd55c756
filizzola <juliofilizzola@hotmail.com>
2021-09-25 19:10:08 -0300

    reinstalando pacotes do material -iu'

--- src/components/MoviesRow.jsx
+++ src/components/MoviesRow.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './style/MoviesRow.css';
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 function MoviesRow({title, items}) {
   return (
     <div className="movieRow">
       <h1>{title}</h1>
+      <div className="movieRow--left" >
+        <NavigateBeforeIcon style={{ fontSize: 50 }}/>
+      </div>
+      <div className="movieRow--right" >
+        <NavigateNextIcon  style={{ fontSize: 50 }}/>
+      </div>
+
       <div className="movieRow--listearea">
       <div className="movieRow--list">
         {items.results.length > 0 && items.results.map((item, index) => (
