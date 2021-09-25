551d6db74cb8798edd52229001b9a489f9c655b7
filizzola <juliofilizzola@hotmail.com>
2021-09-25 17:33:06 -0300

    estilizando o header e colocando uma imagem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import LogoNetflix from "./img/netflixLogo.png";
 
 import "./style/header.css";
 
 const Header = () => {
   return (
-    <header className="">
-      ola
+    <header>
+      <div className="header--logo">
+        <a href="/">
+          <img src={ LogoNetflix } alt="Logo da Netflix"/>
+        </a>
+      </div>
+      <div className="header--user">
+        <a href="/">
+          <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="User"/>
+        </a>
+      </div>
     </header>
   )
 }
