f8c14e725cb6ef2d941959f2379e7e539f2d9ad7
filizzola <juliofilizzola@hotmail.com>
2021-09-25 21:22:44 -0300

    criando logica de filmes para esquerda | começo da logica para filmes a esquerda'

--- src/components/MoviesRow.jsx
+++ src/components/MoviesRow.jsx
@@ -4,18 +4,37 @@ import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 function MoviesRow({title, items}) {
+  const [scrollX, setScrollX] = React.useState(-400)
+
+  const handleLeftArrow = () => {
+      let valueScrollX = scrollX + Math.round(window.innerWidth / 2);
+      if (valueScrollX > 0) {
+        valueScrollX = 0;
+      };
+      setScrollX(valueScrollX);
+  };
+
+  const handleRightArrow = () => {
+    let valueScrollX = scrollX - Math.round(window.innerWidth / 2);
+    let list
+    setScrollX(valueScrollX);
+  };
+
   return (
     <div className="movieRow">
       <h1>{title}</h1>
-      <div className="movieRow--left" >
+      <div className="movieRow--left" onClick={handleLeftArrow} >
         <NavigateBeforeIcon style={{ fontSize: 50 }}/>
       </div>
-      <div className="movieRow--right" >
+      <div className="movieRow--right" onClick={handleRightArrow} >
         <NavigateNextIcon  style={{ fontSize: 50 }}/>
       </div>
 
       <div className="movieRow--listearea">
-      <div className="movieRow--list">
+      <div className="movieRow--list" style={{
+        marginLeft: scrollX,
+        width: items.results.length * 150
+      }}>
         {items.results.length > 0 && items.results.map((item, index) => (
           <div className="movieRow--item" key={index}>
 
